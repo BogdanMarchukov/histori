d541efdbc00f12566c34b8b2898a071ece086ffa
bogdan_info <bogdan_info@mail.ru>
2022-02-09 19:11:54 +0300

    add components in BurgerMenu
    1. add Avatar
    2. add menu navigation

--- Components/BurgerMenu/BurgerMenu.tsx
+++ Components/BurgerMenu/BurgerMenu.tsx
@@ -1,11 +1,23 @@
 import React, {useState} from 'react'
 import classes from './burgerMenu.module.css'
 import MenuIcon from '@mui/icons-material/Menu';
-import {Drawer} from "@material-ui/core";
+import {Avatar, Drawer} from "@material-ui/core";
+import {RootState} from "../../redux/redusers/indexReduser";
+import {useSelector} from "react-redux";
+import {avatarImgSrc} from "../../redux/action-creators/rootFunction";
 
 type Props = {}
 const BurgerMenu = (props: Props) => {
     const [openDrawer, setOpenDrawer] = useState(false)
+
+    const selector = (state: RootState) => {
+        return {
+            avatarSrc: state.homePageReducer.pathAvatar
+        }
+    }
+
+    const {avatarSrc} = useSelector(selector)
+    const pathAvatar = process.env.NEXT_PUBLIC_IMG_AVATAR_PATCH ?? ''
 
     return (
         <>
@@ -24,6 +36,24 @@ const BurgerMenu = (props: Props) => {
                         onClose={() => setOpenDrawer(false)}
                     >
                         <div className={classes.drawerMenu}>
+                            <div className={classes.drawerAvatar}>
+                                <Avatar
+                                    src={avatarImgSrc(pathAvatar, avatarSrc!)}
+                                    alt="Аватар"
+                                    sx={{width: 100, height: 100}}
+                                />
+                            </div>
+                                <nav className={classes.drawerContent}>
+                                    <span>Личный кабинет</span>
+                                    <hr/>
+                                    <ul>
+                                        <li>Главная</li>
+                                        <li>История</li>
+                                        <li>Общество</li>
+                                        <li>Право</li>
+                                        <li>Экономика</li>
+                                    </ul>
+                                </nav>
 
                         </div>
                     </Drawer>
